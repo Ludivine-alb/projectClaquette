3ca89cf21e47e7cf5f45eb358fae25fea371bd35
AxelDrouillard <axel.drouillard@gmail.com>
2022-03-16 13:57:00 +0100

    Test

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Document</title>
+    <title>Claquette Project</title>
 </head>
 <body>
-    
+    <nav class="navbar">
+        <div class="navbar__logo">Logo</div>
 </body>
 </html>

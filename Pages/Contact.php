17ed34d3b8ecccad29e19bc27b50081ef1617864
agorden <arthur.gorden@live.fr>
2022-03-16 17:35:19 +0100

    add faq.php and contact-faq-style.css and html structure and start css

---
+++ Pages/Contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Contact the Claquette Project</title>
+  <meta name="description" content="description" />
+  <meta name="author" content="author" />
+  <meta name="keywords" content="keywords" />
+  <link rel="stylesheet" href="/Assets/contact-faq-style.css" type="text/css" />
+</head>
+
+<body>
+  <!-- <?php include '/layouts/header.php' ?> -->
+
+  <h1>Contact the Claquette Project</h1>
+
+  <!-- <?php include '/layouts/footer.php' ?> -->
+</body>
+
+</html>
---
+++ Pages/faq.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foire aux Qlaquettes</title>
+    <meta name="description" content="description" />
+    <meta name="author" content="author" />
+    <meta name="keywords" content="keywords" />
+    <link rel="stylesheet" href="/Assets/contact-faq-style.css" type="text/css" />
+</head>
+
+<body>
+    <!-- <?php include '/layouts/header.php' ?> -->
+
+    <h1>Foire aux Qlaquettes</h1>
+
+    <!-- <?php include '/layouts/footer.php' ?> -->
+</body>
+
+</html>

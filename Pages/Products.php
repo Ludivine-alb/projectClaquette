762c542fe8fd965eeeca2b395c7500920e6e42bb
Ludivine Alberola <ludivine.alb@gmail.com>
2022-03-17 09:26:09 +0100

    Modifications files

--- Pages/Products.php
+++ Pages/Products.php
@@ -5,10 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="products.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:wght@100&display=swap" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <h1>Ca git a tout va.</h1>
-    <p></p>
+    <header>
+</header>
+
+    <h1>Produits</h1>
+    
+   <div><img class="Claquette1" src="/Assets/Images/Claquette.jpeg" alt="paire de claquette"></div>
+    <p>Modèle 1</p>  
+    <li>Matériaux : plip plop plop</li>
+    <li>Taille : Universelle</li>
+    <li>Conception :</li>
+    <input type="button" value="Add">
+
+    <img src="/Assets/Images/Claquettas.jpeg" alt="paire de claquette">
+    <p>Modèle 2</p>
+    <li>Matériaux :</li>
+    <li>Taille :</li>
+    <li>Conception :</li>
+    <input type="button" value="Add">
+    
+    <img src="/Assets/Images/Claquettes3.jpeg" alt="paire de claquette">
+    <p>Modèle 3</p>
+    <li>Matériaux :</li>
+    <li>Taille :</li>
+    <li>Conception :</li>
+    <input type="button" value="Add">
 </body>
 </html>

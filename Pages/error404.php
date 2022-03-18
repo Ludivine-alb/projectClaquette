8150497db9cf61c5e7a3fe7f49569cc7401718db
agorden <arthur.gorden@live.fr>
2022-03-18 10:32:19 +0100

    faq and error page almost done, images and icons added to assets

---
+++ Pages/error404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html class="error-page">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Error 404 - Claquette Project</title>
+    <meta name="description" content="description" />
+    <meta name="author" content="author" />
+    <meta name="keywords" content="keywords" />
+    <link rel="stylesheet" href="/Assets/contact-faq-style.css" type="text/css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Serif:wght@100|Raleway">
+</head>
+
+<body>
+    <!-- Header : included in PHP -->
+    <!-- <?php include '/layouts/header.php' ?> -->
+
+    <main>
+        <h1 class="error-title">Error 404</h1>
+
+        <section>
+            <h2>Message from the Claquette Project</h2>
+            <p>Please try clicking below.</p><br />
+            <div class="error-button-container">
+                <a href="/Pages/Contact.php">
+                    <button id="runAwayButton" type="button">CLICK ME TO GO BACK</button>
+                </a>
+            </div>
+        </section>
+    </main>
+
+    <!-- Footer : included in PHP -->
+    <!-- <?php include '/layouts/footer.php' ?> -->
+</body>
+
+</html>
--- Pages/faq.php
+++ Pages/faq.php
@@ -8,14 +8,73 @@
     <meta name="author" content="author" />
     <meta name="keywords" content="keywords" />
     <link rel="stylesheet" href="/Assets/contact-faq-style.css" type="text/css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Serif:wght@100|Raleway">
 </head>
 
 <body>
+    <!-- Header : included in PHP -->
     <!-- <?php include '/layouts/header.php' ?> -->
 
-    <h1>Foire aux Qlaquettes</h1>
+    <main>
+        <h1 class="mainTitle faq-heading">Foire aux Qlaquettes</h1>
 
+        <section class="faq-container">
+
+            <div class="faq-one">
+                <!-- faq question -->
+                <h2 class="faq-page">Avez-vous vraiment besoin de claquettes ?</h2>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Enfin l'ami, pourquoi vous posez-vous la question ? La réponse est évidemment oui ! Avec un grand OUI !</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+
+            <div class="faq-two">
+                <!-- faq question -->
+                <h2 class="faq-page">Avez-vous changé d'avis ?</h2>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Vous êtes inconstants, mais n'est-ce pas le propre des gens merveilleux tels que vous ?</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+
+            <div class="faq-three">
+                <!-- faq question -->
+                <h2 class="faq-page">Êtes-vous certain de votre choix et à l'aise avec vous-même ?</h2>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>En effet, la claquette n'est pas un vêtemenent si facile à porter. Cependant, il sera la preuve aux yeux du monde de votre témérité, vous n'aurez pas démérité. Bien que vous le pensiez (car je vous vois venir), ce site internet n'a reçu aucune aide d'un quelconque poète urbain quant à sa confection.</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+
+            <div class="faq-four">
+                <!-- faq question -->
+                <h2 class="faq-page">Une question</h2>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Une réponse</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+
+            <div class="faq-five">
+                <!-- faq question -->
+                <h2 class="faq-page">Quelle a été notre source pour réaliser cette FAQ ?</h2>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>La voici : <a href="https://sweetcode.io/how-to-build-an-faq-page-with-html-and-javascript/" target="_blank">How to build an FAQ page with html and JavaScript ?</a></p>
+                </div>
+            </div>
+            
+        </section>
+    </main>
+
+    <!-- Footer : included in PHP -->
     <!-- <?php include '/layouts/footer.php' ?> -->
+    <script src="/Assets/Main.js"></script>
 </body>
 
 </html>

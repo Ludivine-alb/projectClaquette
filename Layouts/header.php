c250dcd73069b89aef97eeffdc68c55da43b8c6f
AxelDrouillard <axel.drouillard@gmail.com>
2022-03-24 16:43:46 +0100

    home page done

---
+++ Layouts/header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+        <div class="logo">
+            <a href="#"></a>
+            <img src="/Assets/Images/Logo-test.png" alt="logo claquette">
+        </div>
+        <div class="onglets">
+            <a href="/">Accueil</a>
+            <a href="/Pages/Products">Produits</a>
+            <a href="/Pages/Faq">Faq</a>
+            <a href="/Pages/Contact">Contact</a>
+        </div>
+    </nav>
+</header>
